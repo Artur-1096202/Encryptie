8278597b9d130729b80c6937479cd6b2b7ac93c1
Artur(1096202) <1096202@hr.nl>
2025-10-25 16:24:41 +0200

    Add PBKDF2HMAC imports and password_to_key function for Fernet

--- encryptie.py
+++ encryptie.py
@@ -1,7 +1,24 @@
 from cryptography.fernet import Fernet
 import base64
 import os
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 
+def generate_key_from_password(password: str, salt: bytes) -> bytes:
+    # Dit gebruikt PBKDF2HMAC om een sleutel uit het wachtwoord te genereren
+
+    Password_to_key_generator = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=200000,
+        backend=default_backend()
+    )
+    key = Password_to_key_generator.derive(password.encode())
+
+    # Fernet vereist een base64-gecodeerde sleutel
+    return base64.urlsafe_b64encode(key)
 
 def main():
 
